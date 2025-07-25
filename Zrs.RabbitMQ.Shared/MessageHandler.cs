b3524d0f3dc95acae072e615cd5eb58e66fb9772
JLester <ScorpionJL@hotmail.com>
2025-07-25 15:15:05 -0400

    Fix compile issue.  guess I should add a compile check action to PRs

--- Zrs.RabbitMQ.Shared/MessageHandler.cs
+++ Zrs.RabbitMQ.Shared/MessageHandler.cs
@@ -7,7 +7,7 @@ public interface IMessageHandler<T> where T : MessageBase
 
 public class MessageHandler : IMessageHandler<MessageBase>
 {
-    public async Task HandleMessage(MessageBase message)
+    public Task HandleMessage(MessageBase message)
     {
         // Implement the logic to handle the message here
         // For example, you could log the message or process it in some way
