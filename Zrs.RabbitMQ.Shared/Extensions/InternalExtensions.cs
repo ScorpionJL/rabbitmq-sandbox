17c8bf64645d486ac97d4d94c41fb1298a212ad1
JLester <ScorpionJL@hotmail.com>
2025-07-26 14:21:09 -0400

    Renamed functional extensions to internal extensions

--- Zrs.RabbitMQ.Shared/Extensions/InternalExtensions.cs
+++ Zrs.RabbitMQ.Shared/Extensions/InternalExtensions.cs
@@ -3,12 +3,12 @@ using System.Text.Json;
 
 namespace Zrs.RabbitMQ.Shared.Extensions;
 
-internal static class FunctionalExtensions
+internal static class InternalExtensions
 {
     public static byte[] ToUTF8Bytes(this string s) => Encoding.UTF8.GetBytes(s);
     public static string ToUTF8String(this byte[] bytes) => Encoding.UTF8.GetString(bytes);
 
     public static string JsonSerialize<T>(this T obj, JsonSerializerOptions? options = null) => JsonSerializer.Serialize(obj, options);
-     public static T? JsonDeserialize<T>(this string json, JsonSerializerOptions? options = null) => JsonSerializer.Deserialize<T>(json, options);
+    public static T? JsonDeserialize<T>(this string json, JsonSerializerOptions? options = null) => JsonSerializer.Deserialize<T>(json, options);
     public static T? JsonDeserialize<T>(this ReadOnlyMemory<byte> bytes, JsonSerializerOptions? options = null) => JsonSerializer.Deserialize<T>(bytes.Span, options);
 }
